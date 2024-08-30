2beb15bff177faba02d13766f7614f63828c7f13
isosceles45 <atharvasardal06@gmail.com>
2024-08-31 01:48:23 +0530

    signin-bug-fix

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -96,27 +96,27 @@ export const signin = async (req, res, next) => {
         const validUser = await User.findOne({ email });
         if (!validUser) return next(errorHandler(404, "User not found"));
 
-        const validPassword = bcryptjs.compareSync(
-            password,
-            validUser.password
-        );
-        if (!validPassword) return next(errorHandler(401, "wrong credentials"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
 
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
 
-        const { password: hashedPassword, ...rest } = validUser._doc;
-        const expiryDate = new Date(Date.now() + 3600000);
+        const { password: hashedPassword, ...userData } = validUser.toObject();
 
+        const expiryDate = new Date(Date.now() + 3600000);
         res.cookie("access_token", token, {
             httpOnly: true,
             expires: expiryDate,
         })
-            .status(200)
-            .json(rest);
+        .status(200)
+        .json({
+            message: "Signin successful!",
+        });
     } catch (error) {
         next(error);
     }
 };
+
 
 // Signout Controller
 export const signout = (req, res) => {

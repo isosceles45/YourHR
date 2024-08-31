b0d83beee47221c7adbc50ebc13ebcad35f9a2d9
isosceles45 <atharvasardal06@gmail.com>
2024-08-31 18:48:19 +0530

    verify-token-removed

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.listen(3000, () => {
 app.use(
     cors({
         origin: "http://localhost:5173",
-        credentials: true, // Allows cookies to be sent and received
+        credentials: true,
     })
 );
 app.use(express.json());
@@ -33,7 +33,6 @@ app.use(cookieParser());
 
 app.use("/api/user", UserRoutes);
 
-// Error handling middleware
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { updateProfileAndResume, signup, signin, signout } from "../controllers/user.controller.js";
-import { verifyToken } from "../utils/auth.js";
 
 const router = express.Router();
 
@@ -8,6 +7,6 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post("/signout", signout);
-router.post("/profile/upload",verifyToken, updateProfileAndResume);
+router.post("/profile/upload", updateProfileAndResume);
 
 export default router;
